472f88017e61b965762a8115463416eea2224f94
Ty Ruffin <tyruffin@Tys-MacBook-Pro.local>
2013-01-13 16:44:57 -0600

    first files added

---
+++ project2.js
@@ -0,0 +1,20 @@
+// Tyrone Ruffin
+// SDI 1301
+// Project 2
+
+// My Variables
+var Myname =  "Tyrone Ruffin" // String
+var tuitionCost = 60000 // Number
+var sDiProjects = [ 
+    "week1",
+    "week2",
+    "week3",
+    "week4"
+]; // Array
+
+
+// My output
+
+
+
+// My conditionals
