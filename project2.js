8e8d3eb74502190b91462c5cdca715972bb286a3
Ty Ruffin <tyruffin@Tys-MacBook-Pro.local>
2013-01-15 12:06:15 -0600

    third update

--- project2.js
+++ project2.js
@@ -18,8 +18,18 @@ console.log(" Hello Class My name is " +  Myname + ".");
 console.log(" I am currently in SDI class at Full Sail university");
 console.log(" In this class we have 4 main projects.");
 
-
-
-
-
 // My conditionals
+  if (tuitionCost <= 60000) {
+    tuitionCost = tuitionCost - 450;
+    console.log(" If I fail this course I will be paying more than " + tuitionCost + " in tuiton.");
+  } else {
+    console.log(" If I pass this class I will only be paying " + tuitionCost + " in tuition.");
+  }
+
+if(hardCourse === true) {
+     (hardCourse = hardCourse)
+     console.log(" In my opinion it is " + hardCourse + " that this class is pretty hard" + ".");
+} else {
+    console.log(" This class is moderate in difficulty" + ".");
+};
+
